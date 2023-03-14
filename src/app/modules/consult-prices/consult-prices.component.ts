1d25cdfb02564b77ab27b2f9bc466790207e96e0
Bruno Costa <brunoscosta17@gmail.com>
2023-03-14 16:03:40 -0300

    feature: tratando dados de retorno da requisição http

--- src/app/modules/consult-prices/consult-prices.component.ts
+++ src/app/modules/consult-prices/consult-prices.component.ts
@@ -1,6 +1,7 @@
 import { ConsultPricesService } from './../../services/consult-prices.service';
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-consult-prices',
@@ -14,10 +15,12 @@ export class ConsultPricesComponent {
   constructor(private consultPricesService: ConsultPricesService) {}
 
   search(): void {
-    this.queryField.value
     console.log(this.queryField.value);
     this.consultPricesService
       .getPrices(this.queryField.value)
+      .pipe(
+        map(data => data.chart.result[0].indicators.quote[0].open),
+        map(prices => prices.slice(-30)))
       .subscribe({
         next: (data: any) => {
           console.log(data);
--- src/app/services/consult-prices.service.ts
+++ src/app/services/consult-prices.service.ts
@@ -10,7 +10,7 @@ export class ConsultPricesService {
   constructor(private _http: HttpClient) { }
 
   getPrices(asset: string): Observable<any> {
-    const url = `https://query2.finance.yahoo.com/v8/finance/chart/${asset}?range=1mo&interval=1d`;
+    const url = `https://query2.finance.yahoo.com/v8/finance/chart/${asset}?interval=1d&range=1mo&includePrePost=true&lang=en-US&region=US&corsDomain=finance.yahoo.com`;
     return this._http.get<any>(url);
   }
 
